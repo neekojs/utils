94f436107831d44d2253d011bf7bd58a8b7ac459
bincheng.sbc <bincheng.sbc@alibaba-inc.com>
2019-11-10 19:22:58 +0800

    feat: v1.0.2

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import AliYun from './upload/aliyun';
+import QCloud from './upload/qcloud';
+import QiNiu from './upload/qiniu';
+
+const Uploader = {
+    AliYun,
+    QCloud,
+    QiNiu
+};
+
+export default {
+    Uploader
+};
